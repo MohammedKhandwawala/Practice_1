4182b5d449603c4e582da07d0cc5e6c1220d26f9
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2017-02-03 19:06:50 +0530

    Zomato API testing

--- app/src/main/java/com/example/android/practice_1/MainActivity.java
+++ app/src/main/java/com/example/android/practice_1/MainActivity.java
@@ -1,25 +1,35 @@
 package com.example.android.practice_1;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView registerLink, restoreLink;
+    private TextView registerLink, restoreLink, textView7;
     private EditText identityField, passwordField;
-    private Button loginButton;
+    private Button loginButton, fetchButton;
     private CheckBox rememberLoginBox;
     private Button facebookButton;
+
+    private final static String ZOMATO_API_KEY = "8e847da3360a94cf26e381cbce62a41a",
+    CATEGORIES_URL = "https://developers.zomato.com/api/v2.1/geocode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         identityField = (EditText) findViewById( R.id.identityField );
         passwordField = (EditText) findViewById( R.id.passwordField );
         loginButton = (Button) findViewById( R.id.loginButton );
+        fetchButton = (Button) findViewById(R.id.fetchButton);
+        textView7 = (TextView) findViewById(R.id.textView7);
 
 
         String tempString = getResources().getString( R.string.register_text );
@@ -74,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         tempString = getResources().getString( R.string.restore_link );
         underlinedContent = new SpannableString( tempString );
         underlinedContent.setSpan( new UnderlineSpan(), 0, tempString.length(), 0 );
-        restoreLink.setText( underlinedContent );
+       // restoreLink.setText( underlinedContent );
 
         loginButton.setOnClickListener( new View.OnClickListener()
         {
@@ -93,6 +105,65 @@ public class MainActivity extends AppCompatActivity {
                 onRegisterLinkClicked();
             }
         } );
+
+        fetchButton.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View view )
+            {
+                onFetchButtonClicked();
+            }
+        } );
+
+    }
+
+    private void onFetchButtonClicked() {
+
+        new FetchRestaurantsTask().execute();
+
+    }
+
+    private class FetchRestaurantsTask extends AsyncTask<Void, Void, String>{
+        private Exception exception;
+
+        protected void onPreExecute() {
+            textView7.setText("Pre Executing");
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            try {
+                URL url = new URL(CATEGORIES_URL+"?lat=12.9845&lon=80.2330");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("user-key",ZOMATO_API_KEY);
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            Log.i("INFO", response);
+
+            textView7.setText(response);
+        }
     }
 
     public void onLoginButtonClicked()
